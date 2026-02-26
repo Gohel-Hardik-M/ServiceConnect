bedee7d8891f27edcf198be7fe8afaa82da7c6f2
Gohel-Hardik-M <hardikmg21@gmail.com>
2026-02-26 09:09:42 +0530

    starting

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import Login from './login'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <Login/>
-   
+   <Login/>    
   )
 } 
 
---
+++ src/components/header.jsx
@@ -0,0 +1,27 @@
+<>
+<header class="sticky top-0 z-50 w-full bg-white/80 dark:bg-background-dark/80 backdrop-blur-md border-b border-primary/10">
+<div class="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
+<div class="flex items-center gap-2 group cursor-pointer">
+<div class="bg-primary p-2 rounded-lg text-background-dark">
+<span class="material-symbols-outlined block text-2xl">home_repair_service</span>
+</div>
+<h1 class="text-xl font-800 tracking-tight text-background-dark dark:text-white">Service<span class="text-primary">Connect</span></h1>
+</div>
+<nav class="hidden md:flex items-center gap-10">
+<a class="text-sm font-semibold hover:text-primary transition-colors" href="#how-it-works">How it Works</a>
+<a class="text-sm font-semibold hover:text-primary transition-colors" href="our_services.html">Services</a>
+<a class="text-sm font-semibold hover:text-primary transition-colors" href="reviews.html">Reviews</a>
+</nav>
+<div class="flex items-center gap-4">
+<button class="hidden sm:flex text-sm font-bold px-4 py-2 rounded-lg border-2 border-primary/20 hover:border-primary transition-all">
+                <a href="login.html">    Login </a>
+                </button>
+<button class="bg-primary text-background-dark text-sm font-bold px-5 py-2.5 rounded-lg hover:shadow-lg hover:shadow-primary/30 transition-all">
+                  <a href="register.html">  Become Provider</a>
+                </button>
+</div>
+</div>
+</header>
+
+
+</>
